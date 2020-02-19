7ae1bf6303ff6e75da7ca64bd76c4767f8432677
vickeex <727951723@qq.com>
2020-02-19 16:17:04 +0800

    pure shell for auto deploy: execute on each node

--- hadoop_deploy_pure.sh
+++ hadoop_deploy_pure.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
-# if execute in fab command, too many unnecessary connections
-# use shell to install libs locally for each node first
+# Hadoop Deployment on Each Node
+# Test system version: Ubuntu 16.0.4 TLS
+#
+# PARAMETERS:
+# 1> Essential
+# num, master_ip, slaves_ip
+# 2> Optional
+# JAVA_VERSION, HADOOP_VERSION, HADOOP CONFIG (eg, replication_factor)
+# ...
 
-work_dir=$1
+USER=$1
+WORK_DIR=/home/$USER
+MASTER=$2
+SLAVES=$3
 
-# install java
-sudo apt-get update
-sudo apt-get install openjdk-8-jre openjdk-8-jdk -y
-java_home=`dpkg -L openjdk-8-jdk | grep '/bin' | head -1 | sed 's/\/bin//'`
-echo "export JAVA_HOME=$java_home" >> ~/.profile
-echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.profile
-echo "export CLASSPATH=.:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar" >> ~/.profile
+# apt-get
+apt-get update
 
-# install pdsh
-sudo apt-get install pdsh -y
-sudo echo "ssh" > /etc/pdsh/rcmd_default
+# stop ufw
+ufw disable
+
+# date sync
+apt-get install ntp -y
+apt-get install ntpdate -y
+ntpdate -u ntp1.aliyun.com
+
+# install java and configure sys path
+apt-get install openjdk-8-jre openjdk-8-jdk -y
+# shellcheck disable=SC2006
+JAVA_HOME=`dpkg -L openjdk-8-jdk | grep '/bin' | head -1 | sed 's/\/bin//'`
+printf "export JAVA_HOME=%s\nexport PATH=\$JAVA_HOME/bin:\$PATH\nexport CLASSPATH=.:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar\n" "$JAVA_HOME" >> ~/.profile
+
+# install pdsh and set default connection way
+apt-get install pdsh -y
+printf "ssh" > /etc/pdsh/rcmd_default
 
 # install hadoop
-mkdir bigData
-cd bigData
 wget http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-3.1.3/hadoop-3.1.3.tar.gz
 tar -zxvf hadoop-3.1.3.tar.gz
-cd ..
 
 # system env config for hadoop
-echo "export HADOOP_HOME=$work_dir/bigData/hadoop-3.1.3" >> ~/.profile
-echo "export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin" >> ~/.profile
+printf "export HADOOP_HOME=%s/hadoop-3.1.3\nexport PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin\n" "$WORK_DIR" >> $WORK_DIR/.profile
 
 # hadoop env config
-echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> $work_dir/bigData/hadoop-3.1.3/etc/hadoop/hadoop-env.sh
-echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> $work_dir/bigData/hadoop-3.1.3/etc/hadoop/yarn-env.sh
+printf "export JAVA_HOME=%s\n" "$JAVA_HOME" >> $WORK_DIR/hadoop-3.1.3/etc/hadoop/hadoop-env.sh
+printf "export JAVA_HOME=%s\n" "$JAVA_HOME" >> $WORK_DIR/hadoop-3.1.3/etc/hadoop/yarn-env.sh
 
-echo '<?xml version="1.0" encoding="UTF-8"?>
+printf '<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
         <property>
                 <name>fs.defaultFS</name>
-                <value>hdfs://blockchain-001:9000</value>
+                <value>hdfs://%s:9000</value>
         </property>
         <property>
                 <name>io.file.buffer.size</name>
@@ -46,14 +61,14 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
                 <name>hadoop.tmp.dir</name>
                 <value>file:/usr/local/hadoop/tmp</value>
         </property>
-</configuration>' > $work_dir/bigData/hadoop-3.1.3/etc/hadoop/core-site.xml
+</configuration>' "$MASTER"> $WORK_DIR/hadoop-3.1.3/etc/hadoop/core-site.xml
 
-echo '<?xml version="1.0" encoding="UTF-8"?>
+printf '<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
         <property>
                 <name>dfs.namenode.secondary.http-address</name>
-                <value>blockchain-001:9001</value>
+                <value>%s:9001</value>
         </property>
         <property>
                 <name>dfs.namenode.name.dir</name>
@@ -79,9 +94,9 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
                 <name>dfs.web.ugi</name>
                 <value>supergroup</value>
         </property>
-</configuration>' > $work_dir/bigData/hadoop-3.1.3/etc/hadoop/hdfs-site.xml
+</configuration>' "MASTER" > $WORK_DIR/hadoop-3.1.3/etc/hadoop/hdfs-site.xml
 
-echo '<?xml version="1.0"?>
+printf '<?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
         <property>
@@ -90,15 +105,15 @@ echo '<?xml version="1.0"?>
         </property>
         <property>
                 <name>mapreduce.jobhistory.address</name>
-                <value>blockchain-001:10020</value>
+                <value>%s:10020</value>
         </property>
         <property>
                 <name>mapreduce.jobhistory.webapp.address</name>
-                <value>blockchain-001:19888</value>
+                <value>%s:19888</value>
         </property>
-</configuration>' > $work_dir/bigData/hadoop-3.1.3/etc/hadoop/mapred-site.xml
+</configuration>' "MASTER" "MASTER" > $WORK_DIR/hadoop-3.1.3/etc/hadoop/mapred-site.xml
 
-echo '<?xml version="1.0"?>
+printf '<?xml version="1.0"?>
 <configuration>
         <property>
                 <name>yarn.nodemanager.aux-services</name>
@@ -110,35 +125,38 @@ echo '<?xml version="1.0"?>
         </property>
         <property>
                 <name>yarn.resourcemanager.address</name>
-                <value>blockchain-001:8032</value>
+                <value>%s:8032</value>
         </property>
         <property>
                 <name>yarn.resourcemanager.scheduler.address</name>
-                <value>blockchain-001:8030</value>
+                <value>%s:8030</value>
         </property>
         <property>
                 <name>yarn.resourcemanager.resource-tracker.address</name>
-                <value>blockchain-001:8031</value>
+                <value>%s:8031</value>
         </property>
         <property>
                 <name>yarn.resourcemanager.admin.address</name>
-                <value>blockchain-001:8033</value>
+                <value>%s:8033</value>
         </property>
         <property>
                 <name>yarn.resourcemanager.webapp.address</name>
-                <value>blockchain-001:8088</value>
+                <value>%s:8088</value>
         </property>
-</configuration>' > $work_dir/bigData/hadoop-3.1.3/etc/hadoop/yarn-site.xml
+</configuration>' "MASTER" "MASTER" "MASTER" "MASTER" "MASTER" > $WORK_DIR/hadoop-3.1.3/etc/hadoop/yarn-site.xml
 
+printf '%s' "$SLAVES" > $WORK_DIR/hadoop-3.1.3/etc/hadoop/workers
 
-# stop ufw
-ufw disable
-
-# date sync
-apt-get install ntp -y
-apt-get install ntpdate -y
-ntpdate -u ntp1.aliyun.com
-
-# 'source ~/.profile'
 # source cannot be recognized in dash shell (default)
-# execute in fab commands
+source $WORK_DIR/.profile
+
+#    if env.user == 'root':
+#        local(
+#            'sed -i "2i HDFS_DATANODE_USER=root\nHDFS_DATANODE_SECURE_USER=hdfs\nHDFS_NAMENODE_USER=root\nHDFS_SECONDARYNAMENODE_USER=root" start-dfs.sh stop-dfs.sh')
+#        local(
+#            'sed -i "2i YARN_RESOURCEMANAGER_USER=root\nHADOOP_SECURE_DN_USER=yarn\nYARN_NODEMANAGER_USER=root" start-yarn.sh stop-yarn.sh')
+#
+#    # format
+#    if local—ip==MASTER:
+#        hadoop namenode -format
+#        ./$WORKDIR/hadoop-3.1.3/sbin/start-all.sh
